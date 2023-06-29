a4cb73fd2f4c110d8e05fba4e170eaf473f84480
ali <ali.mozafari733@yahoo.com>
2023-06-29 21:25:53 +0430

    add js select class querySelector

---
+++ index.js
@@ -0,0 +1,6 @@
+let endDate = document.querySelector("input[name='endDate']");
+let clock = document.querySelector(".clock");
+let sec = document.querySelector(".seconds");
+let timeinterval;
+let timestop = true;
+
