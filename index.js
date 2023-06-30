9db6aee03f2cc2badd00df8596f5502d185bc7d0
ali <ali.mozafari733@yahoo.com>
2023-06-30 23:31:55 +0430

    add function limite

--- index.js
+++ index.js
@@ -4,3 +4,27 @@ let sec = document.querySelector(".seconds");
 let timeinterval;
 let timestop = true;
 
+endDate.addEventListener("change", () => {
+    let timeG =endDate.value;
+    clearInterval(timeinterval);
+    limite(timeG);
+  });
+  
+  function limite(s) {
+    function xx() {b
+      let temp = stop(s);
+      if (temp.totale < 0) {
+        clearInterval(timeinterval);
+        
+      }
+      for (let key in temp) {
+        let el = clock.querySelector("." + key);
+        if (el) {
+          el.innerText = temp[key];
+        }
+      }
+    }
+  
+    timeinterval = setInterval(xx, 1000);
+  }
+  
